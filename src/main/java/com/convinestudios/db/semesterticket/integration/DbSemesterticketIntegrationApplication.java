202c924e2b14d0d7f30773fb15fcf36ad9c551e2
Paul <p.limpert@icloud.com>
2025-01-04 17:54:32 +0100

    Initial commit

---
+++ src/main/java/com/convinestudios/db/semesterticket/integration/DbSemesterticketIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.convinestudios.db.semesterticket.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSemesterticketIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbSemesterticketIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/convinestudios/db/semesterticket/integration/DbSemesterticketIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convinestudios.db.semesterticket.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSemesterticketIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
